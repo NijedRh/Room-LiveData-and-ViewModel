6e2164da14e7c38877062fbba2076c31935b250f
Med Nijed Rhaiem <nijed2015@gmail.com>
2020-12-24 22:01:32 +0100

    Initial commit

---
+++ app/src/main/java/com/example/roomwordsample/NewWordActivity.java
@@ -0,0 +1,74 @@
+package com.example.roomwordsample;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class NewWordActivity extends AppCompatActivity {
+    public static final String EXTRA_REPLY =
+            "com.example.android.roomwordssample.REPLY";
+
+
+    private EditText mEditWordView;
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_word);
+        mEditWordView = findViewById(R.id.edit_word);
+
+        final Button button = findViewById(R.id.button_save);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent replyIntent = new Intent();
+                if (TextUtils.isEmpty(mEditWordView.getText())) {
+                    setResult(RESULT_CANCELED, replyIntent);
+                } else {
+                    String word = mEditWordView.getText().toString();
+                    replyIntent.putExtra(EXTRA_REPLY, word);
+                    setResult(RESULT_OK, replyIntent);
+                }
+                finish();
+            }
+        });
+    }
+    /**
+     * Handles the button click to create a new date picker fragment and
+     * show it.
+     *
+     * @param view View that was clicked
+     */
+    public void showDatePicker(View view) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(),"datePicker");
+    }
+    /**
+     * Process the date picker result into strings that can be displayed in
+     * a Toast.
+     *
+     * @param year Chosen year
+     * @param month Chosen month
+     * @param day Chosen day
+     */
+    public void processDatePickerResult(int year, int month, int day) {
+        String month_string = Integer.toString(month + 1);
+        String day_string = Integer.toString(day);
+        String year_string = Integer.toString(year);
+        String dateMessage = (month_string +
+                "/" + day_string +
+                "/" + year_string);
+
+
+        TextView textView =findViewById(R.id.textView);
+        textView.setText("date : " + dateMessage);
+
+    }
+}
---
+++ app/src/main/java/com/example/roomwordsample/WordListAdapter.java
@@ -0,0 +1,73 @@
+package com.example.roomwordsample;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class    WordListAdapter extends RecyclerView.Adapter<WordListAdapter.WordViewHolder> {
+
+    private final LayoutInflater mInflater;
+    private List<Word> mWords;
+
+
+    WordListAdapter(Context context) { mInflater = LayoutInflater.from(context); }
+
+    @Override
+    public WordViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = mInflater.inflate(R.layout.recyclerview_item, parent, false);
+        return new WordViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(WordViewHolder holder, int position) {
+        if (mWords != null) {
+            Word current = mWords.get(position);
+            holder.wordItemView.setText(current.getWord());
+
+        } else {
+            // Covers the case of data not being ready yet.
+            holder.wordItemView.setText("No Word");
+        }
+    }
+
+    void setWords(List<Word> words){
+        mWords = words;
+        notifyDataSetChanged();
+    }
+
+
+
+    // getItemCount() is called many times, and when it is first called,
+    // mWords has not been updated (means initially, it's null, and we can't return null).
+    @Override
+    public int getItemCount() {
+        if (mWords != null)
+            return mWords.size();
+        else return 0;
+    }
+
+    class WordViewHolder extends RecyclerView.ViewHolder {
+        private final TextView wordItemView;
+        private final TextView txt1 ;
+        private final  TextView txt2 ;
+
+
+        private WordViewHolder(View itemView ) {
+            super(itemView);
+
+            wordItemView = itemView.findViewById(R.id.textView);
+            txt1 = itemView.findViewById(R.id.textView3);
+            txt2 = itemView.findViewById(R.id.textview4);
+
+        }
+    }
+    public Word getWordAtPosition (int position) {
+        return mWords.get(position);
+    }
+}
